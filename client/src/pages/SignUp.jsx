ac6dc16f177ce36c46a2a1eb96df13e4f1792714
chesnokov.maksim11 <chesnokov.maksim11@wb.ru>
2024-09-16 14:26:37 +0300

    v2

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -46,11 +46,10 @@ export default function SignUp() {
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
               Chesnokov's
             </span>
-            Blog
+            NIRS
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
-            or with Google.
+            You can sign up with your email and password.
           </p>
         </div>
         {/* right */}
